712b70153b6d985e80f7f2fa88671f47a8319839
Nazar Palamarchuk <ua.nazar.palamarchuk.ua@gmail.com>
2024-09-18 01:02:40 +0300

    feat: Add fetchExchangeRates function and ExchangeRateResponseSchema
    This commit adds the fetchExchangeRates function to fetch exchange rates from an API and the ExchangeRateResponseSchema to validate the response data. The fetchExchangeRates function uses the API key stored in the environment variables to make the API call and returns the parsed exchange rate data. If the API call fails or the response data is invalid, an error is thrown.

---
+++ lib/fetchExchangeRates.ts
@@ -0,0 +1,34 @@
+import { ExchangeRateResponseSchema } from "@/model/ExchangeRateResponseSchema"
+
+export async function fetchExchangeRates() {
+	const apiKey = process.env.EXCHANGE_RATE_API_KEY
+
+	if (!apiKey) {
+		throw new Error("API key not configured")
+	}
+
+	const res = await fetch(
+		`https://v6.exchangerate-api.com/v6/${apiKey}/latest/USD`,
+		{
+			// Next.js fetch options for caching and revalidation
+			next: {
+				revalidate: 5, // Revalidate data every 5 seconds
+			},
+		}
+	)
+
+	if (!res.ok) {
+		throw new Error("Failed to fetch exchange rates")
+	}
+
+	const data = await res.json()
+
+	// Validate the response using Zod
+	const parsedData = ExchangeRateResponseSchema.safeParse(data)
+
+	if (!parsedData.success) {
+		throw new Error("Invalid data format")
+	}
+
+	return parsedData.data
+}
