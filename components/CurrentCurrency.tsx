7b799a73c4feb51f397f630bf1502e9d9b1836f0
Nazar Palamarchuk <ua.nazar.palamarchuk.ua@gmail.com>
2024-09-19 02:23:22 +0300

    refactor: Improve error handling in CurrentCurrency component

--- components/CurrentCurrency.tsx
+++ components/CurrentCurrency.tsx
@@ -1,10 +1,20 @@
 import { fetchExchangeRates } from "@/lib/fetchExchangeRates"
 import { getExchangeRate } from "@/lib/utils"
-import type { CurrencyCode } from "@/model/ExchangeRateResponseSchema"
+import type {
+	CurrencyCode,
+	ExchangeRateResponse,
+} from "@/model/ExchangeRateResponseSchema"
 
 export const CurrentCurrency = async () => {
-	// Fetch exchange rates data
-	const exchangeRates = await fetchExchangeRates()
+	// Fetch exchange rates data or return null if an error occurs
+	let exchangeRates: ExchangeRateResponse
+	try {
+		exchangeRates = await fetchExchangeRates()
+	} catch (error) {
+		// As there is no place to display the error message, we just return null
+		// The fetchExchangeRates handles the error and throws an error message for a developer to see
+		return null
+	}
 
 	// Base currency for conversion
 	const baseCurrency: CurrencyCode = "UAH"
