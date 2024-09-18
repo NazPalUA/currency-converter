ca91d8b4b9de4444cbdecb3107d380c492bbb6f7
Nazar Palamarchuk <ua.nazar.palamarchuk.ua@gmail.com>
2024-09-18 22:16:36 +0300

    refactor: Update Header component with logo image
    - Import the Image component from "next/image" to use a logo image in the Header component.
    - Add the logo image to the Link component in the Header, displaying it alongside the text.
    - Adjust the styling of the logo image to fit within a rounded container.

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,6 @@
+import Image from "next/image"
 import Link from "next/link"
+import Logo from "../public/logo.png"
 import { Container } from "./Container"
 import { CurrentCurrency } from "./CurrentCurrency"
 import { ModeToggle } from "./ModeToggle"
@@ -7,7 +9,12 @@ export const Header = () => {
 	return (
 		<header className="top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur sticky">
 			<Container className="flex justify-between">
-				<Link href="/" className="text-2xl sm:text-3xl lg:text-4xl">
+				<Link href="/" className="text-2xl sm:text-3xl lg:text-4xl flex gap-2">
+					<Image
+						src={Logo}
+						alt="Currency Converter"
+						className="w-8 sm:w-9 lg:w-10  h-8 sm:h-9 lg:h-10 rounded-full object-fit object-center"
+					/>
 					<span className="text-blue-500">Currency</span>
 					<span className="text-yellow-400">{" Converter"}</span>
 				</Link>
