247dd1b961db4c1eeea8b2f7c7d2e635519794f1
Nazar Palamarchuk <ua.nazar.palamarchuk.ua@gmail.com>
2024-09-19 02:23:13 +0300

    refactor: Improve error handling for fetching exchange rates in Home component
    - Added try-catch block to handle errors during fetching exchange rates in the Home component.
    - If an error occurs, an error message is thrown and caught by the error boundary to display to the user.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
 import { Container } from "@/components/Container"
 import { CurrencyConverter } from "@/components/CurrencyConverter"
 import { fetchExchangeRates } from "@/lib/fetchExchangeRates"
+import { ExchangeRateResponse } from "@/model/ExchangeRateResponseSchema"
 
 export default async function Home() {
-	const exchangeRatesData = await fetchExchangeRates()
+	let exchangeRatesData: ExchangeRateResponse
+
+	try {
+		exchangeRatesData = await fetchExchangeRates()
+	} catch (error) {
+		// Throw an error during render if exchange rates are missing.
+		// The message will be caught by error boundary and displayed to the user.
+		throw new Error("Failed to get exchange rates")
+	}
 
 	return (
 		<main className="min-h-full">
